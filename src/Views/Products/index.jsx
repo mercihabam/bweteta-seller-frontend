ad1f87edc38fb98c5c04ad883d14345bec48a8d7
Merci Jacob <mercihabam@gmail.com>
2021-06-12 16:33:02 +0200

    add btn to add prduct

--- src/Views/Products/index.jsx
+++ src/Views/Products/index.jsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
 import { AllProducts } from "./all";
 
 
 function Products(){
+    const [ active, setActive ] = useState("all")
+
+    const categorys = [
+        "Habillement",
+        "Appareils electroniques",
+        "Cosmetiques",
+        "Accessoires"
+    ]
 
     return(
-        <div className="products">Produits
+        <div className="products">
+            <div className="div-categorys">
+                <div onClick={() =>setActive("all")} className={`category ${active === "all"? "active":null}`}>Tous les produits</div>
+                {
+                    categorys.map((category) =>(
+                        <div onClick={() =>setActive(category)} className={`category ${active === category? "active":null}`}> {category} </div>
+                    ))
+                }
+            </div>
             <div>
                 <AllProducts />
+            </div>
+            <div className="btn-add-prod">
+                <button className="btn-add">Créer un produit</button>
             </div>
         </div>
     );
