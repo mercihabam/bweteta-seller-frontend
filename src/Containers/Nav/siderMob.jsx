e5c1eb8546501f0adfbe17c47c04cc97344f2030
Merci Jacob <mercihabam@gmail.com>
2021-06-21 22:35:16 +0200

    update ui

--- src/Containers/Nav/siderMob.jsx
+++ src/Containers/Nav/siderMob.jsx
@@ -7,17 +7,33 @@ import logo from "./img/min_logo.png";
 export function SideBarMob(props){
     const history = useHistory();
 
+    const toggleSider = () =>{
+        const sider = document.querySelector(".sidebar-mob");
+        const divSider = document.querySelector(".nav-mob-sider");
+        if(sider && divSider){
+            sider.style.transform = "translateX(-40%)";
+            setTimeout(() =>{
+                divSider.style.display = "none"
+            }, [300]);
+        }
+    };
+
+    const goTo = (path) =>{
+        toggleSider();
+        history.push(path)
+    }
+
     return(
         <ProSidebar toggled={true} className="sidebar-mob">
             <div className="sidelogo">
                 <img src={logo} alt="" className="logo" />
             </div>
             <Menu popperArrow={true} iconShape="circle">
-                <MenuItem onClick={()=>history.push("/overview")} icon={<DashboardOutlined />}>Tableau de bord</MenuItem>
-                <MenuItem onClick={()=>history.push("/products")} icon={<AppstoreOutlined />}>Produits</MenuItem>
-                <MenuItem onClick={()=>history.push("/orders")} icon={<DollarOutlined />}>Commandes</MenuItem>
-                <MenuItem onClick={()=>history.push("/myshop")} icon={<ShopOutlined />}>Mon shop</MenuItem>
-                <MenuItem onClick={()=>history.push("/settings")} icon={<SettingOutlined />}>Parametres</MenuItem>
+                <MenuItem onClick={()=>goTo("/overview")} icon={<DashboardOutlined />}>Tableau de bord</MenuItem>
+                <MenuItem onClick={()=>goTo("/products")} icon={<AppstoreOutlined />}>Produits</MenuItem>
+                <MenuItem onClick={()=>goTo("/orders")} icon={<DollarOutlined />}>Commandes</MenuItem>
+                <MenuItem onClick={()=>goTo("/myshop")} icon={<ShopOutlined />}>Mon shop</MenuItem>
+                <MenuItem onClick={()=>goTo("/settings")} icon={<SettingOutlined />}>Parametres</MenuItem>
             </Menu>
         </ProSidebar>
     );
